ca38364f0aeb43eac10b047257018b7ff30cf7e4
Galyna Oliynik <ole_gal@ukr.net>
2023-12-19 11:18:55 +0200

    Commit Project Animal

---
+++ src/main/java/task1/Animal.java
@@ -0,0 +1,33 @@
+/**
+ * Створити нову базу даних за допомогою MySQLWorkbench.
+ * Створити звичайний java-проєкт та під'єднати до нього бібліотеки JPA та Hibernate.
+ * Створити файл з налаштуваннями persistence.xml у теці META-INF.
+ * Створити клас Animal (int age, String name, boolean tail) з методами get та set як сутність до нашої таблиці.
+ * За допомогою jpa зробити crud у класі AnimalHelper.
+ */
+
+
+package task1;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class Animal {
+    @Id
+    @GeneratedValue
+    int id;
+    int age;
+    String name;
+    boolean tail;
+}
+
